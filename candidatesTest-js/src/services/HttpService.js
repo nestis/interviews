d7fe12f3c5e26166b103a78090aea6179374067b
Nestor <pearljamsp@gmail.com>
2018-04-08 19:01:48 +0100

    vue: Improve HttpService. POST method.

--- candidatesTest-js/src/services/HttpService.js
+++ candidatesTest-js/src/services/HttpService.js
@@ -3,11 +3,28 @@ import Vue from 'vue';
 class HttpService {
 
     get(url) {
+        return new Promise((resolve, reject) => {
+            fetch(url, {
+                method: 'GET'
+            }).then(response => {
+                let res;
+                if (response.status === 200) {
+                    resolve(response.json());
+                } else {
+                    reject(response);
+                }
+            }, error => reject(error.json()))
+        });
+    }
+
+    post(url, body) {
         return fetch(url, {
-            method: 'GET'
+            headers: { 'Content-Type': 'application/json'},
+            method: 'POST',
+            body: JSON.stringify(body)
         }).then(response => response.json(),
             error => error.json())
-    }   
+    }
 }
 export default new HttpService()
 
