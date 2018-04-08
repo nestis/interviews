5f948de59f28990221e429a5d3e916a92b283327
Nestor <pearljamsp@gmail.com>
2018-04-08 19:05:48 +0100

    java: added @Field to Answer id

--- questions-microservice/src/main/java/com/nestis/interview/questions/entity/Answer.java
+++ questions-microservice/src/main/java/com/nestis/interview/questions/entity/Answer.java
@@ -1,10 +1,13 @@
 package com.nestis.interview.questions.entity;
 
+import org.springframework.data.mongodb.core.mapping.Field;
+
 import lombok.Data;
 
 @Data
 public class Answer {
 	
+	@Field("id")
 	private Integer id;
 	
 	private String text;
