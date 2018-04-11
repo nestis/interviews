71f9fce140f5348bec4ded145dee4171c2704f6e
Nestor <pearljamsp@gmail.com>
2018-04-11 17:21:12 +0100

    go: fixes obtaining gateway config fixes importing gateway change listen port

--- gateway-go/src/nestis/API-gateway/gateway/gateway.go
+++ gateway-go/src/nestis/API-gateway/gateway/gateway.go
@@ -10,31 +10,19 @@ import (
 	"time"
 )
 
-var gatewayMap map[string]string
-
-type endpoint struct {
+type gateway struct {
 	Endpoints map[string]string `json:"endpoints"`
 }
+
+var gatewayMap gateway
 
 // init Initialites the Gateway
 func init() {
 	log.Println("Getting config from Config Server...")
 
-	// Get the profile environment variable
-	profile, found := os.LookupEnv("profile")
-	if !found {
-		// Try to get the uppercase environment variable name
-		profileUpper, foundUpper := os.LookupEnv("PROFILE")
-		if !foundUpper {
-			log.Println("Profile environment variable hasn´t been found. Setting profile to dev.")
-			profile = "dev"
-		} else {
-			profile = profileUpper
-		}
-	}
+	configURL, profile := getEnvProperties()
 
-	// Build config URL
-	url := "http://localhost:9000/gateway-" + strings.ToLower(profile) + ".json"
+	url := configURL + "/gateway-" + strings.ToLower(profile) + ".json"
 
 	// Create Http client with 5s timeout
 	var netClient = &http.Client{
@@ -48,25 +36,45 @@ func init() {
 	}
 	defer resp.Body.Close()
 
-	var endpoints endpoint
-	json.NewDecoder(resp.Body).Decode(&endpoints)
-
-	gatewayMap = endpoints.Endpoints
+	json.NewDecoder(resp.Body).Decode(&gatewayMap)
 
 	log.Println("Config obtained!")
 }
 
+func getEnvProperties() (string, string) {
+	// Get the profile environment variable
+	profile, found := os.LookupEnv("profile")
+	if !found {
+		// Try to get the uppercase environment variable name
+		profileUpper, foundUpper := os.LookupEnv("PROFILE")
+		if !foundUpper {
+			log.Println("profile environment variable hasn´t been found. Setting profile to dev.")
+			profile = "dev"
+		} else {
+			profile = profileUpper
+		}
+	}
+
+	// Get config URL
+	configURL, found := os.LookupEnv("configUrl")
+	if !found {
+		log.Println("configUrl environment variable hasn´t been found. Setting url to http://localhost:9000")
+		configURL = "http://localhost:9000"
+	}
+
+	return configURL, profile
+}
+
 // HandleRequest Handles requests and redirect them to the proper server
 func HandleRequest(w http.ResponseWriter, request *http.Request) {
-	url := request.URL.String()
+	url := strings.Replace(request.URL.String(), "/", "", 1)
 	reqMethod := request.Method
 
 	header := request.Header["User-Agent"]
 	log.Println("Received " + request.RemoteAddr + ": " + reqMethod + " " + url + " - " + header[0])
 
 	client := &http.Client{}
-
-	for key, value := range gatewayMap {
+	for key, value := range gatewayMap.Endpoints {
 		if key == url {
 			// Dispatch rest request to value
 			log.Println("Dispatching request to " + value)
--- gateway-go/src/nestis/API-gateway/server.go
+++ gateway-go/src/nestis/API-gateway/server.go
@@ -4,11 +4,11 @@ import (
 	"log"
 	"net/http"
 
-	"nestis/API-gateway/gateway"
+	"nestis/api-gateway/gateway"
 )
 
 func main() {
 	http.HandleFunc("/", gateway.HandleRequest)
 	log.Println("API Gateway up!")
-	log.Fatal(http.ListenAndServe(":8090", nil))
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
